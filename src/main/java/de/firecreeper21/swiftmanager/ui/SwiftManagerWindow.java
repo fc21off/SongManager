c9a642800e3cb9773e8f921870b4c9b8e0f22b9e
st197974 <st197974@stud.uni-stuttgart.de>
2026-01-25 22:04:31 +0100

    Commit - Lots of Changes lol 2

--- src/main/java/de/firecreeper21/swiftmanager/ui/SwiftManagerWindow.java
+++ src/main/java/de/firecreeper21/swiftmanager/ui/SwiftManagerWindow.java
@@ -9,7 +9,6 @@ import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.util.Collections;
 import java.util.List;
 
 public class SwiftManagerWindow extends JFrame{
